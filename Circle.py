7b5b512b4dbfd6a8b8ddd970a4b979141f208564
Samuel McKean <36656703+samuelmckean@users.noreply.github.com>
2018-03-07 00:21:58 -0600

    Add files via upload

---
+++ Circle.py
@@ -0,0 +1,27 @@
+# import all data fields and methods from superclass
+from GeometricObject import GeometricObject 
+import math
+
+class Circle(GeometricObject):
+    # construct a circle object
+    def __init__(self, radius=1):
+        super().__init__()
+        self.__radius = radius
+
+    def getRadius(self):
+        return self.__radius
+
+    def getPerimeter(self):
+        return 2 * self.__radius * math.pi
+
+    def getArea(self):
+        return self.__radius ** 2 * math.pi
+
+    def setRadius(self, radius):
+        self.__radius = radius
+
+    def getDiameter(self):
+        return 2 * self.__radius
+
+    def printCircle(self):
+        print(self.__str__() + "radius: " + str(self.__radius))
